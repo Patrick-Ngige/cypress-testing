e119c3bc2b6441f59c5ed2a84924f503c814d4ea
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-28 14:28:12 +0300

    preparing materials for visual validations

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -2,5 +2,10 @@
 import * as todoPage from '../page-objects/todo-page'
 
 describe('visual validation', () => {
+    before(() => todoPage.navigate())
 
+    it('should look good', () => {
+        todoPage.addTodo('Using Cypress')
+        todoPage.addTodo('Using Applitools')
+    })
 })
