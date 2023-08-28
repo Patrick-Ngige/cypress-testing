40cad798f383b14bebcae0da5e35d31c421e540e
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-28 12:43:33 +0300

    validating the length of the 'Active', 'Completed' and 'All'

--- cypress/integration/todomvc-filtering-spec.js
+++ cypress/integration/todomvc-filtering-spec.js
@@ -17,4 +17,16 @@ describe('filtering', () => {
 
         cy.get('.todo-list li').should('have.length', 2)
     })
+
+    it('should filter "Completed" todos', () => {
+        cy.contains('Completed').click()
+
+        cy.get('.todo-list li').should('have.length', 1)
+    })
+
+    it('should filter "All" todos', () => {
+        cy.contains('All').click()
+
+        cy.get('.todo-list li').should('have.length', 3)
+    })
 })
