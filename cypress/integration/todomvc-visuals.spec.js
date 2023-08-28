9c58ced5abf5291eb35eee39df5b183b4e80d2c8
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-28 18:34:04 +0300

    beforeEach and afterEach to take the screenshots

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -4,6 +4,9 @@ import * as todoPage from '../page-objects/todo-page'
 describe('visual validation', () => {
     before(() => todoPage.navigate())
 
+    beforeEach(() => cy.eyesOpen({appName: 'Cypress Testing', batchName: 'Using Applitools'}))
+    afterEach(() => cy.eyesClose())
+
     it('should look good', () => {
         cy.eyesCheckWindow('empty todo list')
 
