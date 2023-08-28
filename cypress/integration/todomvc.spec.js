14c8a4f8c6dbfd152b9f155e216548a94c206639
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-28 11:46:12 +0300

    testing the clear completed button

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -6,4 +6,6 @@ it('should be able to add a new todo to the list', () => {
     cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
 
     cy.get('.toggle').click()
+
+    cy.contains('Clear completed')
 })
