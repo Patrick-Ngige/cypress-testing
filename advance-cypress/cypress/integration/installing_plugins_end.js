c053cd92b8d541485721ab926df7f17c0f9767f2
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-30 00:59:11 +0300

    applitools installation

--- advance-cypress/cypress/integration/installing_plugins_end.js
+++ advance-cypress/cypress/integration/installing_plugins_end.js
@@ -2,21 +2,15 @@
 
 it('Installing plugins', () => {
 
-  cy
-    .visit('/');
+  cy.visit('/');
 
-    cy
-    .eyesOpen({
-      appName: 'Trello app',
-      testName: 'Trying out Applitools plugin',
-      browser: { width: 800, height: 600 },
-    });
+  cy.eyesOpen({
+    appName: 'Trello app',
+    testName: 'Trying out Applitools plugin',
+    browser: { width: 800, height: 600 }
+  });
 
-  cy
-    .eyesCheckWindow('Board list');
+  cy.eyesCheckWindow('Board list');
 
-  cy
-    .eyesClose();
-
-
+  cy.eyesClose();
 });
