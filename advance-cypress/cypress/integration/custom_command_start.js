fce8e53b0a30ec6b36fd881f7400a7ecbaf0d3dc
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-29 18:39:08 +0300

    creating custom command

--- advance-cypress/cypress/integration/custom_command_start.js
+++ advance-cypress/cypress/integration/custom_command_start.js
@@ -4,17 +4,22 @@ beforeEach(() => {
   cy.request('POST', '/api/reset')
 })
 
+Cypress.Commands.add('addBoard', () => {
+  cy
+  .get('[data-cy="create-board"]')
+  .click()
+
+  cy
+  .get('[data-cy=new-board-input]')
+  .type('new board{enter}')
+})
+
 it('Custom commands', () => {
 
   cy
     .visit('/');
 
   cy
-    .get('[data-cy="create-board"]')
-    .click();
-
-  cy
-    .get('[data-cy=new-board-input]')
-    .type('new board{enter}');
+  .addBoard()
 
 });
