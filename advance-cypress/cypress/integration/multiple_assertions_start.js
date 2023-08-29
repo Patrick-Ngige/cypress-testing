9d8fcdc2cc6645a3da51d72ab264cff9267af73f
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-29 16:30:04 +0300

    multiple assertions in cypress

--- advance-cypress/cypress/integration/multiple_assertions_start.js
+++ advance-cypress/cypress/integration/multiple_assertions_start.js
@@ -10,12 +10,10 @@ it('Multiple assertions', () => {
 
   cy
     .get('[data-cy=task]')
-    .eq(0)
-    .should('contain.text', 'bread')
-
-  cy
-    .get('[data-cy=task]')
-    .eq(1)
-    .should('contain.text', 'milk')
+    .then( item => {
+      expect(item[0]).to.contain.text('milk')
+      expect(item[1]).to.contain.text('bread')
+    })
+  
 
 })
