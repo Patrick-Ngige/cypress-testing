67eab26df08a82b04e3b72c9c79b40ff53f29ab6
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-29 16:35:48 +0300

    adding conditional check and error message

--- advance-cypress/cypress/integration/multiple_assertions_start.js
+++ advance-cypress/cypress/integration/multiple_assertions_start.js
@@ -11,6 +11,9 @@ it('Multiple assertions', () => {
   cy
     .get('[data-cy=task]')
     .should( item => {
+      if (item.length !== 3) {
+        throw new Error('Not enough elements!')
+      }
       expect(item[0]).to.contain.text('milk')
       expect(item[1]).to.contain.text('bread')
     })
