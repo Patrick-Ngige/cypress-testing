dd8ebfdf58bb97a3ae2fb5372041b7a4ca152b23
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-29 16:56:53 +0300

    triggering the mouse event and testing if they work correctly

--- advance-cypress/cypress/integration/changing_DOM_start.js
+++ advance-cypress/cypress/integration/changing_DOM_start.js
@@ -10,8 +10,18 @@ beforeEach(() => {
 it('Changing the DOM', () => {
 
   cy
-    .get('[data-cy="star"]')
-    .invoke('show')
-    .click()
+    .get('[data-cy="board-item"]')
+    .trigger('mouseover')
 
+  cy
+    .get('[data-cy=star]')
+    .should('be.visible')
+
+  cy
+    .get('[data-cy="board-item"]')
+    .trigger('mouseout')
+
+  cy
+    .get('[data-cy=star]')
+    .should('not.be.visible')
 })
