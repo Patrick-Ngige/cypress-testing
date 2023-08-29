a8e4154d2d72a8af89fd8e103736f8e5cc55392b
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-29 23:56:19 +0300

    installing plugins

--- advance-cypress/cypress/integration/installing_plugins_start.js
+++ advance-cypress/cypress/integration/installing_plugins_start.js
@@ -5,4 +5,17 @@ it('Installing plugins', () => {
   cy
     .visit('/board/77787127477');
 
+  cy
+  .get('[data-cy=task]')
+  .eq(0)
+  .as('task2')
+
+  cy
+  .get('[data-cy=task]')
+  .eq(1)
+  .as('task1')
+
+  cy
+  .get('@task2')
+  .drag('@task1')
 });
