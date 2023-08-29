e622d185040a7761aecd288732761804b1cb31aa
Patrick-Ngige <wakemanjajr@gmail.com>
2023-08-30 00:46:14 +0300

    applitools installation

--- advance-cypress/cypress/integration/installing_plugins_start.js
+++ advance-cypress/cypress/integration/installing_plugins_start.js
@@ -3,15 +3,23 @@
 it('Installing plugins', () => {
 
   cy
-    .visit('board/20028236889');
+    .visit('/');
 
   cy
-  .get('[data-cy=task]')
-  .eq(0)
-  .click()
-
-  cy
-  .get('.dropzone')
-  .attachFile('logo.png', {subjectType: 'drag-n-drop'})
+  .get('[data-cy=board-item]')
+  .realHover('have.css', 'background-color', 'rgb(5, 90, 140)')
   
+  cy
+  .eyesOpen({
+    appName: 'Cypress testing',
+    testName: 'JavaScript test',
+    browser: {width: 800, height: 600},
+  });
+
+  cy
+  .eyesCheckWindow('Main Page')
+
+  cy
+  .eyesClose()
+
 });
